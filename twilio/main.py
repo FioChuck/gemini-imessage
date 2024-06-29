17234fcd147687bd5f762fabf5dc08fdc5d159f0
FiocChuck <chasf@google.com>
2024-06-29 19:43:01 -0400

    threading

--- twilio/main.py
+++ twilio/main.py
@@ -3,35 +3,14 @@ import threading
 import functions_framework
 from functions import *
 from compute import *
-import requests
-from dotenv import load_dotenv
+from datetime import datetime
 
 
-def download_text_from_gcs(project_id, bucket_name, file_name):
-    """Downloads text content from a file in GCS."""
+def print_message_received():
+    """Prints a message with the current timestamp."""
 
-    storage_client = storage.Client(project=project_id)  # Specify project ID
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(file_name)
-
-    text_content = blob.download_as_text()
-
-    print(
-        f"File {file_name} downloaded from bucket {bucket_name} in project {project_id}")
-    return text_content
-
-
-def upload_text_to_gcs(project_id, bucket_name, file_name, text_content):
-    """Uploads text content to a file in GCS."""
-
-    storage_client = storage.Client(project=project_id)  # Specify project ID
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(file_name)
-
-    blob.upload_from_string(text_content, content_type='text/plain')
-
-    print(
-        f"File {file_name} uploaded to bucket {bucket_name} in project {project_id}")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")
+    print(f"Message received at: {timestamp}")
 
 
 @functions_framework.http
@@ -53,41 +32,10 @@ def main(request):
     # Set CORS headers for the main request
     headers = {"Access-Control-Allow-Origin": "*"}
 
-    url = os.getenv('URL')
+    # Call the function to print the message
+    print_message_received()
 
-    payload = json.dumps({
-        "limit": 1,
-        "offset": 0,
-        "chatGuid": "SMS;-;+16504046137",
-        "with": [
-            "chat",
-            "chat.participants",
-            "attachment",
-            "handle",
-            "sms"
-        ],
-        "sort": "DESC"
-    })
-    headers = {
-        'Content-Type': 'application/json'
-    }
-
-    response = requests.request("POST", url, headers=headers, data=payload)
-
-    response_dict = response.json()
-
-    project_id = "fiorenza-house-hunt"  # Replace with your actual project ID
-    bucket_name = "cf-imessage-status"
-    file_name = "status.txt"
-
-    text_content = download_text_from_gcs(project_id, bucket_name, file_name)
-
-    if text_content != response_dict["data"][0]["text"] and response_dict["data"][0]["isFromMe"] is False:
-        print("new message received!")
-        print("message says: " + response_dict["data"][0]["text"])
-
-        text_content = str(response_dict["data"][0]["text"])
-
-        upload_text_to_gcs(project_id, bucket_name, file_name, text_content)
+    thread = threading.Thread(target=run, kwargs={})
+    thread.start()
 
     return ("done", 200, headers)
---
+++ twilio/process.py
@@ -0,0 +1,81 @@
+import json
+
+from functions import *
+from compute import *
+import requests
+
+
+def download_text_from_gcs(project_id, bucket_name, file_name):
+    """Downloads text content from a file in GCS."""
+
+    storage_client = storage.Client(project=project_id)  # Specify project ID
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    text_content = blob.download_as_text()
+
+    print(
+        f"File {file_name} downloaded from bucket {bucket_name} in project {project_id}")
+    return text_content
+
+
+def upload_text_to_gcs(project_id, bucket_name, file_name, text_content):
+    """Uploads text content to a file in GCS."""
+
+    storage_client = storage.Client(project=project_id)  # Specify project ID
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    blob.upload_from_string(text_content, content_type='text/plain')
+
+    print(
+        f"File {file_name} uploaded to bucket {bucket_name} in project {project_id}")
+
+
+def run(**kwargs):
+
+    try:
+
+        url = os.getenv('URL')
+
+        payload = json.dumps({
+            "limit": 1,
+            "offset": 0,
+            "chatGuid": "SMS;-;+16504046137",
+            "with": [
+                "chat",
+                "chat.participants",
+                "attachment",
+                "handle",
+                "sms"
+            ],
+            "sort": "DESC"
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        response_dict = response.json()
+
+        project_id = "fiorenza-house-hunt"  # Replace with your actual project ID
+        bucket_name = "cf-imessage-status"
+        file_name = "status.txt"
+
+        text_content = download_text_from_gcs(
+            project_id, bucket_name, file_name)
+
+        if text_content != response_dict["data"][0]["text"] and response_dict["data"][0]["isFromMe"] is False:
+            print("-------------------------------")
+            print("new message received!")
+            print("message says: " + response_dict["data"][0]["text"])
+            print("-------------------------------")
+
+            text_content = str(response_dict["data"][0]["text"])
+
+            upload_text_to_gcs(project_id, bucket_name,
+                               file_name, text_content)
+
+    except Exception as e:
+        print(e)
