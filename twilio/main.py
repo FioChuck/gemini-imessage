fecce3ab4489400497a3aca731705b6e1aff177f
FiocChuck <chasf@google.com>
2024-06-29 19:30:48 -0400

    new message test

--- twilio/main.py
+++ twilio/main.py
@@ -7,6 +7,33 @@ import requests
 from dotenv import load_dotenv
 
 
+def download_text_from_gcs(project_id, bucket_name, file_name):
+    """Downloads text content from a file in GCS."""
+
+    storage_client = storage.Client(project=project_id)  # Specify project ID
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    text_content = blob.download_as_text()
+
+    print(
+        f"File {file_name} downloaded from bucket {bucket_name} in project {project_id}")
+    return text_content
+
+
+def upload_text_to_gcs(project_id, bucket_name, file_name, text_content):
+    """Uploads text content to a file in GCS."""
+
+    storage_client = storage.Client(project=project_id)  # Specify project ID
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    blob.upload_from_string(text_content, content_type='text/plain')
+
+    print(
+        f"File {file_name} uploaded to bucket {bucket_name} in project {project_id}")
+
+
 @functions_framework.http
 def main(request):
 
@@ -47,17 +74,20 @@ def main(request):
 
     response = requests.request("POST", url, headers=headers, data=payload)
 
-    print(response.text)
+    response_dict = response.json()
 
-# print("starting threaded app")
+    project_id = "fiorenza-house-hunt"  # Replace with your actual project ID
+    bucket_name = "cf-imessage-status"
+    file_name = "status.txt"
 
-# thread = threading.Thread(target=run, kwargs={
-#     'dtype': dtype,
-#     'body': body,
-#     'num_media': data["NumMedia"],
-#     'sms_sid': data["SmsSid"],
-#     'sms_from': data["From"],
-#     'media_url': url})
-# thread.start()
+    text_content = download_text_from_gcs(project_id, bucket_name, file_name)
+
+    if text_content != response_dict["data"][0]["text"] and response_dict["data"][0]["isFromMe"] is False:
+        print("new message received!")
+        print("message says: " + response_dict["data"][0]["text"])
+
+        text_content = str(response_dict["data"][0]["text"])
+
+        upload_text_to_gcs(project_id, bucket_name, file_name, text_content)
 
     return ("done", 200, headers)
